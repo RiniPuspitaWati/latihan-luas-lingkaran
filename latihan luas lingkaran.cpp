034d81b31f2fb241ddefffb27debc62baafe2b33
RiniPuspitaWati <112569358+RiniPuspitaWati@users.noreply.github.com>
2023-04-09 19:24:26 +0700

    Add files via upload

---
+++ latihan luas lingkaran.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	float luas,pi,r;
+	cout<<" menghitung luas lingkaran \n";
+	cout<<" masukan pi : ";
+	cin>>pi;
+	cout<<" masukan r : ";
+	cin>>r;
+	luas=pi*r*r;
+	
+	cout<<" luas lingkaran adalah "<<luas;
+	return 0;
+}
